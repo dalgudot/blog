e70cbe147e7f92db268377575e18350b0224abe3
dalgudot <dalgudot@gmail.com>
2022-01-12 15:21:04 +0900

    new mac test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,7 @@ import type { NextPage } from 'next';
 import TextEditorBlock from '../components/text-editor/text-editor-block';
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <TextEditorBlock test='test' />
-    </>
-  );
+  return <TextEditorBlock test='test' />;
 };
 
 export default Home;
