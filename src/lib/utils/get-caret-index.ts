8c7dead524a108b27d3e9ddaa7e5e7853bd5b1aa
dalgudot <dalgudot@gmail.com>
2022-01-26 09:40:26 +0900

    edit getCaretIndex func

--- src/lib/utils/get-caret-index.ts
+++ src/lib/utils/get-caret-index.ts
@@ -1,13 +1,15 @@
-export const getCaretIndex = (element) => {
-  let position = 0;
+export const getCaretIndex = (element: HTMLElement) => {
+  let position;
   const isSupported = typeof window.getSelection !== 'undefined';
+
   if (isSupported) {
     const selection = window.getSelection();
+
     if (selection?.rangeCount !== 0) {
       const range = selection?.getRangeAt(0);
       const preCaretRange = range?.cloneRange();
       preCaretRange?.selectNodeContents(element);
-      preCaretRange?.setEnd(range?.endContainer, range?.endOffset);
+      range && preCaretRange?.setEnd(range.endContainer, range.endOffset);
       position = preCaretRange?.toString().length;
     }
   }
