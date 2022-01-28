6fe3b2a3446d4b198e10e66c0ff6e42ca4ab8e96
dalgudot <dalgudot@gmail.com>
2022-01-28 20:24:46 +0900

    add useIsAdmin

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,8 @@
+import { useToast } from '@dalgu/react-toast';
 import type { NextPage } from 'next';
+import { useIsAdmin } from '../lib/hooks/useIsAdmin';
 import { setUid } from '../redux-toolkit/slices/user-slice';
-import {
-  RootState,
-  useAppDispatch,
-  useAppSelector,
-} from '../redux-toolkit/store';
+import { useAppDispatch } from '../redux-toolkit/store';
 import {
   Authentication,
   IAuthentication,
@@ -12,16 +10,19 @@ import {
 } from '../service/firebase/authentication';
 
 const Login: NextPage = () => {
+  const { isAdmin } = useIsAdmin();
   const auth: IAuthentication = new Authentication();
-  const { user, uid } = useAppSelector((state: RootState) => state.user);
   const dispatch = useAppDispatch();
+  const { showToast } = useToast();
 
   const onLogIn = (providerName: TproviderName) => {
     auth //
       .logIn(providerName)
       .then((data) => {
         dispatch(setUid(data.user.uid));
-        if (data.user.uid === process.env.NEXT_PUBLIC_ADMIN_UID) {
+        if (data.user.uid !== process.env.NEXT_PUBLIC_ADMIN_UID) {
+          onLogOut();
+          showToast('관리자가 아니므로 자동으로 로그아웃되었습니다.');
         }
       })
       .catch((error) => {
@@ -41,7 +42,7 @@ const Login: NextPage = () => {
       });
   };
 
-  if (user) {
+  if (isAdmin) {
     return <button onClick={onLogOut}>로그아웃</button>;
   } else {
     return <button onClick={() => onLogIn('Google')}>구글로 로그인</button>;
