7ab69297db17f3d98c68ebd0b10164581aa27a03
dalgudot <dalgudot@gmail.com>
2022-02-12 21:30:52 +0900

    finish focus-caret-after-clipText

--- src/lib/utils/editable-block/paste.ts
+++ src/lib/utils/editable-block/paste.ts
@@ -7,92 +7,71 @@ export const paste = (
   eachRef: MutableRefObject<any>
 ) => {
   navigator.clipboard.readText().then((clipText) => {
-    // const htmlClipText = clipText
-    //   ?.replace(/&/g, '&amp;') // &부터 해야 뒤쪽 <, > replace에 영향 없음!
-    //   .replace(/</g, '&lt;')
-    //   .replace(/>/g, '&gt;');
-
     // Selection의 anchor는 텍스트 선택을 시작한 지점, focus는 선택을 끝낸 지점
+    let newHtml = '';
     const selection = window.getSelection();
     const smallOffset =
-      selection &&
-      ((selection?.anchorOffset < selection?.focusOffset
-        ? selection?.anchorOffset
-        : selection?.focusOffset) as number);
+      selection && Math.min(selection.anchorOffset, selection.focusOffset);
     const largeOffset =
-      selection &&
-      ((selection?.anchorOffset < selection?.focusOffset
-        ? selection?.focusOffset
-        : selection?.anchorOffset) as number);
-
-    console.log(smallOffset, largeOffset);
+      selection && Math.max(selection.anchorOffset, selection.focusOffset);
+    // console.log(smallOffset, largeOffset);
 
     const isNotNullOffset = largeOffset !== null && smallOffset !== null;
     const rangeLength = isNotNullOffset && largeOffset - smallOffset;
     const isSelectionPaste = rangeLength !== false && rangeLength !== 0;
 
-    let newHtml = '';
+    // 문자열을 배열로
+    const tempEachBlockStateTextArray = tempEachBlockStateText
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>')
+      .replace(/&amp;/g, '&')
+      .split('');
+    // 길이 동기화시키기 위해 reverse replace
+    // console.log('before', tempEachBlockStateTextArray);
 
-    // 드래그된 텍스트 있으면 해당 텍스트 지우고 가장 붙여넣기
     if (isSelectionPaste) {
-      const tempEachBlockStateTextArray = tempEachBlockStateText
-        .replace(/&lt;/g, '<')
-        .replace(/&gt;/g, '>')
-        .replace(/&amp;/g, '&')
-        .split('');
-      // console.log('before', tempEachBlockStateTextArray);
-
+      // // 드래그된 텍스트 있으면 해당 텍스트 지우고 가장 붙여넣기 > selection 있으면 그 영역 삭제
       isNotNullOffset &&
-        tempEachBlockStateTextArray.splice(
-          smallOffset,
-          largeOffset - smallOffset
-        );
-
-      smallOffset !== null &&
-        tempEachBlockStateTextArray.splice(smallOffset, 0, clipText);
-
-      // console.log('after', tempEachBlockStateTextArray);
-
-      const selectionRemovedtempEachBlockStateText =
-        tempEachBlockStateTextArray.join('');
-
-      // &부터 해야 뒤쪽 <, > replace에 영향 없음!
-      newHtml = `${selectionRemovedtempEachBlockStateText}`
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;');
-
-      // selection?.getRangeAt(0).insertNode(document.createTextNode(newHtml));
-    } else {
-      const htmlClipText = clipText
-        ?.replace(/&/g, '&amp;') // &부터 해야 뒤쪽 <, > replace에 영향 없음!
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;');
-
-      newHtml = `${tempEachBlockStateText}${htmlClipText}`;
+        tempEachBlockStateTextArray.splice(smallOffset, rangeLength);
     }
+
+    // 붙여넣기
+    smallOffset !== null &&
+      tempEachBlockStateTextArray.splice(smallOffset, 0, clipText);
+
+    // 배열을 문자열로
+    const selectionRemovedtempEachBlockStateText =
+      tempEachBlockStateTextArray.join('');
+
+    // &부터 해야 뒤쪽 <, > replace에 영향 없음!
+    newHtml = `${selectionRemovedtempEachBlockStateText}`
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
 
     setPasteData(newHtml);
-
-    // setPasteData(newHtml); 이후에
-    if (isSelectionPaste) {
-      // console.log(eachRef.current);
-      const targetNode = eachRef.current.childNodes[0];
-      // console.log('targetNode', targetNode);
-
-      console.log(clipText, '///', newHtml);
-      const newCaretPosition =
-        smallOffset !== null && smallOffset + clipText.length;
-      // replace 처리된 htmlClipText가 아닌 clipText 원본으로 해야 length 정확히 맞음
-      // console.log('newCaretPosition', newCaretPosition);
-
-      const newRange = document.createRange();
-      // !== null 해야 0일 때도 작동 -> 0은 false!
-      newCaretPosition && newRange.setStart(targetNode, newCaretPosition); // 앞쪽 셀렉션 지점(smallOffset)에서 붙여넣는 텍스트 길이만큼 뒤쪽에 커서 위치
-      // smallOffset && newRange.setEnd(targetNode, smallOffset);
-
-      selection && selection.removeAllRanges();
-      selection && selection.addRange(newRange);
-    }
+    focusCaretAfterClipText(eachRef, smallOffset, clipText, selection);
+    // setPasteData 데이터 업데이트 이후에 caret 위치 조정
   });
+};
+
+const focusCaretAfterClipText = (
+  eachRef: MutableRefObject<any>,
+  smallOffset: number | null,
+  clipText: string,
+  selection: Selection | null
+) => {
+  const targetNode = eachRef.current.childNodes[0];
+  const newCaretPosition =
+    smallOffset !== null && smallOffset + clipText.length;
+  // !== null 해야 0일 때도 작동 -> 0은 false!
+  // replace 처리된 htmlClipText가 아닌 clipText 원본으로 해야 length 정확히 맞음
+
+  const newRange = document.createRange();
+  newCaretPosition && newRange.setStart(targetNode, newCaretPosition);
+  newCaretPosition && newRange.setEnd(targetNode, newCaretPosition);
+  // 앞쪽 셀렉션 지점(smallOffset)에서 붙여넣는 텍스트 길이만큼 뒤쪽에 커서 위치
+
+  selection && selection.removeAllRanges();
+  selection && selection.addRange(newRange);
 };
