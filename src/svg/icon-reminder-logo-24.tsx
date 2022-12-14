df233d92099b17bc5c9b78ce400a47f283a870bc
dalgudot <dalgudot@gmail.com>
2022-12-14 20:57:58 +0900

    update profile app link

---
+++ src/svg/icon-reminder-logo-24.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react';
+
+const IconReminderLogo24: FC = () => {
+  return (
+    <svg
+      width='1024'
+      height='1024'
+      viewBox='0 0 1024 1024'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect width='1024' height='1024' fill='white' />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M426 770C426 817.496 464.504 856 512 856C559.496 856 598 817.496 598 770H426Z'
+        fill='#3F86D6'
+      />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M569 204C569 204 569.001 204 569.001 204C569.001 203.996 569.001 203.993 569.001 203.989C569.001 172.515 543.481 147 512 147C480.635 147 455.186 172.329 455.001 203.644C473.032 199.215 491.884 196.865 511.287 196.865C531.187 196.865 550.507 199.337 568.957 203.989H569V204Z'
+        fill='#3F86D6'
+      />
+      <path
+        d='M289.14 606.791C289.14 302.957 432.843 215.709 512.5 212C592.156 215.709 735.86 302.957 735.86 606.791C785.202 642.916 864.085 750 745.665 750H279.335C160.914 750 239.798 642.916 289.14 606.791Z'
+        fill='url(#paint0_linear_55_2)'
+      />
+      <defs>
+        <linearGradient
+          id='paint0_linear_55_2'
+          x1='362.5'
+          y1='276'
+          x2='949.5'
+          y2='1121.5'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#54B9FF' />
+          <stop offset='1' stopColor='#0F0D77' stopOpacity='0.7' />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default IconReminderLogo24;
