208b8fea094e52fed6752e19543b0a0022398f16
dalgudot <dalgudot@gmail.com>
2023-06-08 09:04:55 +0900

    update bottom-floating

--- src/components/bottom-floating-button/BottomFloatingButton.tsx
+++ src/components/bottom-floating-button/BottomFloatingButton.tsx
@@ -18,19 +18,17 @@ export default function BottomFloatingButton() {
 
   return (
     <a
-      href={TUMBLBUG_FUNDING_LINK}
+      href={REMINDER_APPSTORE_LINK}
       target='_blank'
       rel='noreferrer'
       className={s.fixed__positon}
       onClick={sendMixpanelEvent}
     >
       <div className={s.left__side}>
-        <IconYoonSeulLogo24 />
+        <IconReminderLogo24 />
         <div className={s.left__side__text}>
-          <h6 className='body2__500'>커리어 에세이</h6>
-          <p className='body4__400'>
-            과학 기자에서 IT 프로덕트 디자이너가 되기까지
-          </p>
+          <h6 className='body2__500'>작은 앱 프로젝트</h6>
+          <p className='body3__400'>리마인더 - 오직 필요한 기능만</p>
         </div>
       </div>
 
@@ -42,22 +40,3 @@ export default function BottomFloatingButton() {
 //
 // 아래는 '작은 앱 프로젝트' return
 //
-// return (
-//   <a
-//     href={REMINDER_APPSTORE_LINK}
-//     target='_blank'
-//     rel='noreferrer'
-//     className={s.fixed__positon}
-//     onClick={sendMixpanelEvent}
-//   >
-//     <div className={s.left__side}>
-//       <IconReminderLogo24 />
-//       <div className={s.left__side__text}>
-//         <h6 className='body2__500'>작은 앱 프로젝트</h6>
-//         <p className='body3__400'>리마인더 - 오직 필요한 기능만</p>
-//       </div>
-//     </div>
-
-//     <IconNewTap24 color='var(--g1)' />
-//   </a>
-// );
